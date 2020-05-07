634e73a7978758e2f1dd38fd338f6e705a7e2b97
akikalia <akika16@freeuni.edu.ge>
2020-05-07 15:22:40 +0400

    sudoku final touch

--- Sudoku/src/SudokuTest.java
+++ Sudoku/src/SudokuTest.java
@@ -2,7 +2,7 @@ import junit.framework.TestCase;
 
 public class SudokuTest extends TestCase {
 
-    String originalEasy = "1 6 4 0 0 0 0 0 2\n" +
+    private String originalEasy = "1 6 4 0 0 0 0 0 2\n" +
             "2 0 0 4 0 3 9 1 0\n" +
             "0 0 5 0 8 0 4 0 7\n" +
             "0 9 0 0 0 6 5 0 0\n" +
@@ -12,7 +12,7 @@ public class SudokuTest extends TestCase {
             "0 7 3 5 0 9 0 0 1\n" +
             "4 0 0 0 0 0 6 7 9";
 
-    String solutionEasy = "1 6 4 7 9 5 3 8 2\n" +
+    private String solutionEasy = "1 6 4 7 9 5 3 8 2\n" +
             "2 8 7 4 6 3 9 1 5\n" +
             "9 3 5 2 8 1 4 6 7\n" +
             "3 9 1 8 7 6 5 2 4\n" +
@@ -21,7 +21,7 @@ public class SudokuTest extends TestCase {
             "8 1 9 6 4 7 2 5 3\n" +
             "6 7 3 5 2 9 8 4 1\n" +
             "4 5 2 3 1 8 6 7 9";
-    String solutionMedium = "5 3 4 6 7 8 9 1 2\n" +
+    private String solutionMedium = "5 3 4 6 7 8 9 1 2\n" +
             "6 7 2 1 9 5 3 4 8\n" +
             "1 9 8 3 4 2 5 6 7\n" +
             "8 5 9 7 6 1 4 2 3\n" +
@@ -30,7 +30,7 @@ public class SudokuTest extends TestCase {
             "9 6 1 5 3 7 2 8 4\n" +
             "2 8 7 4 1 9 6 3 5\n" +
             "3 4 5 2 8 6 1 7 9";
-    String solutionHard = "3 7 5 1 6 2 4 8 9\n" +
+    private String solutionHard = "3 7 5 1 6 2 4 8 9\n" +
             "8 6 1 4 9 3 5 2 7\n" +
             "2 4 9 7 8 5 1 6 3\n" +
             "4 9 3 8 5 7 6 1 2\n" +
@@ -39,7 +39,7 @@ public class SudokuTest extends TestCase {
             "6 5 7 9 2 1 3 4 8\n" +
             "1 8 2 5 3 4 9 7 6\n" +
             "9 3 4 6 7 8 2 5 1";
-    String solutionHardEdited = "3 5 7 1 6 2 4 8 9\n" +
+    private String solutionHardEdited = "3 5 7 1 6 2 4 8 9\n" +
             "8 6 1 4 9 3 2 7 5\n" +
             "2 4 9 7 8 5 1 6 3\n" +
             "4 9 3 8 5 7 6 1 2\n" +
@@ -49,7 +49,7 @@ public class SudokuTest extends TestCase {
             "7 1 4 5 3 8 9 2 6\n" +
             "9 3 2 6 7 4 8 5 1";
 
-    String hardGridEdited = "3 0 0 0 0 0 0 8 0" +
+    private String hardGridEdited = "3 0 0 0 0 0 0 8 0" +
             "0 0 1 0 9 3 0 0 0" +
             "0 4 0 7 8 0 0 0 3" +
             "0 9 3 8 0 0 0 1 2" +
@@ -58,7 +58,7 @@ public class SudokuTest extends TestCase {
             "6 0 0 0 2 1 0 4 0" +
             "0 0 0 5 3 0 9 0 0" +
             "0 3 0 0 0 0 0 5 1";
-    Sudoku s1, s2, s3, s4;
+    private Sudoku s1, s2, s3, s4;
 
     protected void setUp() throws Exception {
 
